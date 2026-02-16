9947f66c8ab8aa705043f05a81d80ed2e13fcf6c
Nastia Mazur <anastasiamazur2006@gmail.com>
2026-02-16 22:33:12 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,283 @@
-/* eslint-disable max-len */
-/* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { UserWarning } from './UserWarning';
+import {
+  createTodo,
+  deleteTodo,
+  getTodos,
+  updateTodo,
+  USER_ID,
+} from './api/todos';
+import { Todo } from './types/Todo';
 
-const USER_ID = 0;
+import { TodoList } from './components/TodoList';
+import { FooterTodo } from './components/FooterTodo';
+import { Errors } from './components/Errors';
+import { HeaderTodo } from './components/HeaderTodo';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = React.useState<Todo[]>([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [sorted, setSorted] = useState('all');
+  const [title, setTitle] = useState('');
+  const [isAdding, setIsAdding] = useState(false);
+  const [tempTodo, setTempTodo] = useState<Todo | null>(null);
+  const [loadingIds, setLoadingIds] = useState<number[]>([]);
+  const [editingId, setEditingId] = useState<number | null>(null);
+
+  const todoInputRef = React.useRef<HTMLInputElement>(null);
+  const activeTodos = todos.filter(t => !t.completed).length;
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const trimmedTitle = title.trim();
+
+    if (!trimmedTitle) {
+      setErrorMessage('Title should not be empty');
+
+      return;
+    }
+
+    setIsAdding(true);
+
+    const newTodoData = {
+      userId: USER_ID,
+      title: trimmedTitle,
+      completed: false,
+    };
+
+    setTempTodo({
+      id: 0,
+      ...newTodoData,
+    });
+
+    createTodo(newTodoData)
+      .then(newTodoFromServer => {
+        setTodos(currentTodos => [...currentTodos, newTodoFromServer]);
+        setTitle('');
+      })
+      .catch(() => {
+        setErrorMessage('Unable to add a todo');
+        todoInputRef.current?.focus();
+      })
+      .finally(() => {
+        setTempTodo(null);
+        setIsAdding(false);
+        setTimeout(() => {
+          todoInputRef.current?.focus();
+        }, 0);
+      });
+  };
+
+  const onDelete = (todoId: number) => {
+    setLoadingIds(prev => [...prev, todoId]);
+
+    deleteTodo(todoId)
+      .then(() => {
+        setTodos(currentTodos =>
+          currentTodos.filter(todo => todo.id !== todoId),
+        );
+      })
+      .catch(() => {
+        setErrorMessage('Unable to delete a todo');
+      })
+      .finally(() => {
+        setLoadingIds(prev => prev.filter(id => id !== todoId));
+      });
+  };
+
+  const onDeleteAll = () => {
+    const todoComplete = todos.filter(todo => todo.completed);
+    const todoCompleteId = todoComplete.map(todo => todo.id);
+
+    setLoadingIds(prev => [...prev, ...todoCompleteId]);
+
+    todoComplete.map(todo => {
+      return deleteTodo(todo.id)
+        .then(() => {
+          setTodos(currentTodos => currentTodos.filter(t => t.id !== todo.id));
+        })
+        .catch(() => {
+          setErrorMessage('Unable to delete a todo');
+        })
+        .finally(() => {
+          setLoadingIds(prev => prev.filter(id => id !== todo.id));
+        });
+    });
+  };
+
+  const todoStatus = (todo: Todo) => {
+    setLoadingIds(prev => [...prev, todo.id]);
+    setErrorMessage('');
+    updateTodo({ ...todo, completed: !todo.completed })
+      .then(updatedTodo => {
+        setTodos(currentTodos =>
+          currentTodos.map(t => (t.id === todo.id ? updatedTodo : t)),
+        );
+      })
+      .catch(() => {
+        setErrorMessage('Unable to update a todo');
+      })
+      .finally(() => {
+        setLoadingIds(prev => prev.filter(id => id !== todo.id));
+      });
+  };
+
+  const handleToggleAll = () => {
+    const allCompleted = todos.every(t => t.completed);
+    const targetStatus = !allCompleted;
+
+    const todosToUpdate = todos.filter(todo => todo.completed !== targetStatus);
+
+    todosToUpdate.forEach(todo => {
+      setLoadingIds(prev => [...prev, todo.id]);
+
+      updateTodo({ ...todo, completed: targetStatus })
+        .then(updatedTodo => {
+          setTodos(currentTodos =>
+            currentTodos.map(t => (t.id === todo.id ? updatedTodo : t)),
+          );
+        })
+        .catch(() => {
+          setErrorMessage('Unable to update a todo');
+        })
+        .finally(() => {
+          setLoadingIds(prev => prev.filter(id => id !== todo.id));
+        });
+    });
+  };
+
+  const renameTodo = (todo: Todo, newTitle: string) => {
+    const trimmedTitle = newTitle.trim();
+
+    if (trimmedTitle === todo.title) {
+      setEditingId(null);
+
+      return;
+    }
+
+    if (!trimmedTitle) {
+      onDelete(todo.id);
+
+      return;
+    }
+
+    setLoadingIds(prev => [...prev, todo.id]);
+
+    updateTodo({ ...todo, title: trimmedTitle })
+      .then(updatedTodo => {
+        setTodos(prev => prev.map(t => (t.id === todo.id ? updatedTodo : t)));
+        setEditingId(null);
+      })
+      .catch(() => {
+        setErrorMessage('Unable to update a todo');
+      })
+      .finally(() => {
+        setLoadingIds(prev => prev.filter(id => id !== todo.id));
+      });
+  };
+
+  const sortedTodoes = todos.filter(todo => {
+    if (sorted === 'active') {
+      return !todo.completed;
+    }
+
+    if (sorted === 'completed') {
+      return todo.completed;
+    }
+
+    return true;
+  });
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .catch(() => setErrorMessage('Unable to load todos'));
+  }, []);
+
+  useEffect(() => {
+    if (todoInputRef.current) {
+      todoInputRef.current.focus();
+    }
+  }, [todos]);
+
+  useEffect(() => {
+    if (errorMessage) {
+      const timer = setTimeout(() => {
+        setErrorMessage('');
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [errorMessage]);
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-add-and-delete#react-todo-app-add-and-delete">
-          React Todo App - Add and Delete
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <HeaderTodo
+          todos={todos}
+          todoInputRef={todoInputRef}
+          title={title}
+          setTitle={setTitle}
+          handleSubmit={handleSubmit}
+          isAdding={isAdding}
+          handleToggleAll={handleToggleAll}
+        />
+
+        <TodoList
+          sortedTodoes={sortedTodoes}
+          onDelete={onDelete}
+          loadingIds={loadingIds}
+          todoStatus={todoStatus}
+          renameTodo={renameTodo}
+          setEditingId={setEditingId}
+          editingId={editingId}
+        />
+
+        {tempTodo && (
+          <div data-cy="Todo" className="todo">
+            <label className="todo__status-label">
+              <input
+                aria-label="Mark todo as completed"
+                type="checkbox"
+                className="todo__status"
+                data-cy="TodoStatus"
+              />
+            </label>
+
+            <span data-cy="TodoTitle" className="todo__title">
+              {tempTodo.title}
+            </span>
+
+            <button type="button" className="todo__remove">
+              ×
+            </button>
+
+            <div data-cy="TodoLoader" className="modal overlay is-active">
+              <div className="modal-background has-background-white-ter" />
+              <div className="loader" />
+            </div>
+          </div>
+        )}
+
+        {todos.length && (
+          <FooterTodo
+            activeTodos={activeTodos}
+            sorted={sorted}
+            setSorted={setSorted}
+            todos={todos}
+            onDeleteAll={onDeleteAll}
+          />
+        )}
+      </div>
+
+      <Errors errorMessage={errorMessage} setErrorMessage={setErrorMessage} />
+    </div>
   );
 };
